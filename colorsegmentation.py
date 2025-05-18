c431e099d8cca5d3d335b6b6005fda10532e159b
CelestialWu <wcx18813195580@outlook.com>
2025-05-18 11:55:37 +0800

    initial commit

---
+++ colorsegmentation.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+target_color_rgb=[
+	(30,67,109), #blue
+	(187,68,47), #red
+]
+
+img=cv2.imread("color_test.jpg")
+hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+
+mask_total=None
+
+for rgb in target_color_rgb:
+	rgb1=np.uint8([[list(rgb)]])
+	hsv1=cv2.cvtColor(rgb1,cv2.COLOR_RGB2HSV)[0][0]
+	
+	lower=np.array([max(hsv1[0]-10,0),50,50])
+	upper=np.array([min(hsv1[0]+10,179),225,225])
+
+	mask=cv2.inRange(hsv,lower,upper)
+
+	mask_total=mask if mask_total is None else cv2.bitwise_or(mask_total,mask)
+
+result=cv2.bitwise_and(img,img,mask=mask_total)
+
+cv2.imwrite("mask_output.png", mask_total)
+cv2.imwrite("result_output.png", result)
+
---
+++ minAreaRect.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+mask=cv2.imread("mask_output.png",cv2.IMREAD_GRAYSCALE)
+
+contours,_=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+image=cv2.imread('color_test.jpg')
+output=image.copy()
+
+for cnt in contours:
+	if cv2.contourArea(cnt)<100:
+		continue
+	
+	rect=cv2.minAreaRect(cnt)
+	box=cv2.boxPoints(rect)
+	box=np.int0(box)
+	cv2.drawContours(output,[box],0,(0,255,0),2)
+	
+	epsilon=0.01*cv2.arcLength(cnt,True)
+	approx=cv2.approxPolyDP(cnt,epsilon,True)
+	cv2.drawContours(output, [approx], 0, (0, 0, 255), 2)
+
+cv2.imwrite('contour_fitted.png', output)
+print("图像已保存为 contour_fitted.png")
+
